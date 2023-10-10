87cd4c2e8f8c788efc1298e2603428fda834ada8
bitf12m033 <umermj2@gmail.com>
2023-10-10 17:58:18 +0500

    Initial commit

---
+++ database/migrations/2023_10_10_112829_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('review');
+            $table->unsignedTinyInteger('rating');
+            $table->timestamps();
+            
+            // $table->unsignedBigInteger('book_id');
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            // Same as above two lines
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
