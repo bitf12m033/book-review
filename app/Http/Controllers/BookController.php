f877b37108e35788d8a946b675fb54a821a781a3
bitf12m033 <umermj2@gmail.com>
2023-10-11 15:36:11 +0500

    60 - Review and Count on all pages
    withReviewsCount()
    withAvgRating()

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -24,13 +24,13 @@ class BookController extends Controller
             return $query->title($title);
         });
 
-        print($filter);
+
         $books = match($filter) {
             'popular_last_month' => $books->popularLastMonth(),
             'popular_last_6monthe' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()->popular()->withAvgRating(),
+            default => $books->latest()->withAvgRating()->withReviewsCount(),
         };
        
         
@@ -66,11 +66,12 @@ class BookController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Book $book)
+    public function show(int $id)
     {
-        $cacheKey = 'book:'.$book->id;
+        $cacheKey = 'book:'.$id;
         
-        $book = cache()->remember($cacheKey , 3600 , fn() => $book->load(['reviews' => fn($query) =>$query->latest()]));
+        $book = cache()->remember($cacheKey , 3600 , 
+                fn() => Book::with(['reviews' => fn($query) =>$query->latest()])->withAvgRating()->withReviewsCount()->findOrFail($id));
         return view('books.show' , ['book' => $book]); //Eager Loading
     }
 
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -59,5 +59,5 @@
         @endforelse
     </ul>
 
-
+    
 @endsection
