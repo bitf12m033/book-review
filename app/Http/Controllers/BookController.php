1c4b681b37cfa2ec727b0d73425107ebe653e5be
bitf12m033 <umermj2@gmail.com>
2023-10-11 15:14:34 +0500

    59 - Invalidating Cache
    To check invalid cache use Events.
    static::updated(fn (Review $review) => cache()->forget('book'.$review->book_id));
            static::deleted(fn (Review $review) => cache()->forget('book'.$review->book_id));
    
    These event triggered with only using Model update only.
    Diretct changes in DB wont trigger these.
    App\Models\Review::where('id',944)->update(['rating' =>2]);

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -24,7 +24,7 @@ class BookController extends Controller
             return $query->title($title);
         });
 
-       
+        print($filter);
         $books = match($filter) {
             'popular_last_month' => $books->popularLastMonth(),
             'popular_last_6monthe' => $books->popularLast6Months(),
@@ -35,10 +35,7 @@ class BookController extends Controller
        
         
         $cacheKey = 'books:'.$filter. ':'.$title;
-        $books = cache()->remember($cacheKey,3600 , function () use($books) {
-           
-            return $books->get();
-        });
+        $books = cache()->remember($cacheKey,3600 , fn() => $books->get());
         return view('books.index',['books' =>$books]);
     }
 
@@ -71,8 +68,10 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-       
-        return view('books.show' , ['book' =>$book->load(['reviews' => fn($query) =>$query->latest()])]); //Eager Loading
+        $cacheKey = 'book:'.$book->id;
+        
+        $book = cache()->remember($cacheKey , 3600 , fn() => $book->load(['reviews' => fn($query) =>$query->latest()]));
+        return view('books.show' , ['book' => $book]); //Eager Loading
     }
 
     /**
